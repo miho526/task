e7d4a3ec02819ab4712e8e96a5631eaed1d09329
miho.takahashi3 <meisuig98@gmail.com>
2020-05-06 02:46:14 +0000

    one_to_many

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = current_user.tasks.all.page(params[:page]).per(3)
   end
 
   def show
@@ -13,14 +13,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に追加されました'
-      redirect_to @task
+      flash[:success] = 'タスクを追加しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が追加されませんでした'
-      render :new
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの追加に失敗しました。'
+      render 'tasks/index'
     end
   end
 
@@ -29,28 +29,30 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスク は正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Taskは更新されませんでした'
+      flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end
   end
 
   def destroy
     @task.destroy
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'タスク は正常に削除されました'
+    redirect_to root_url
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
